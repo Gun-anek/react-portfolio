d3c886061c2546437c27b6f7dde450deede23a56
Gun-anek <gunanek.sir@tokiomarinelife.co.th>
2025-09-01 23:36:30 +0700

    Updates

---
+++ precache-manifest.93b0f86495fe169e9ef8ee68c5ff43d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624648d7add4bc73e43a82abe1a3be72",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c4145df4433aa7648b47",
+    "url": "/react-portfolio/static/css/2.34178e39.chunk.css"
+  },
+  {
+    "revision": "d7f8adbcdecead8b65dc",
+    "url": "/react-portfolio/static/css/main.e424baa4.chunk.css"
+  },
+  {
+    "revision": "c4145df4433aa7648b47",
+    "url": "/react-portfolio/static/js/2.87c2d9d7.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-portfolio/static/js/2.87c2d9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f8adbcdecead8b65dc",
+    "url": "/react-portfolio/static/js/main.ad63d981.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
